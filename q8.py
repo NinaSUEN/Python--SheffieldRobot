bffb8c9f443422814da67ece4a61a303f8060fcb
NinaSUEN <tcsuen1@sheffield.ac.uk>
2019-03-11 15:35:05 +0000

    Create q8.py

---
+++ q8.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+'''COM2009-3009 EV3DEV TEST PROGRAM'''
+
+# Connect left motor to Output C and right motor to Output B
+# Connect an ultrasonic sensor to Input 3
+
+import os
+import sys
+import time
+import ev3dev.ev3 as ev3
+
+from ev3dev.ev3 import *
+from time import sleep
+
+# state constants
+ON = True
+OFF = False
+
+
+def debug_print(*args, **kwargs):
+    '''Print debug messages to stderr.
+    This shows up in the output panel in VS Code.
+    '''
+    print(*args, **kwargs, file=sys.stderr)
+
+
+def reset_console():
+    '''Resets the console to the default state'''
+    print('\x1Bc', end='')
+
+
+def set_cursor(state):
+    '''Turn the cursor on or off'''
+    if state:
+        print('\x1B[?25h', end='')
+    else:
+        print('\x1B[?25l', end='')
+
+
+def set_font(name):
+    '''Sets the console font
+    A full list of fonts can be found with `ls /usr/share/consolefonts`
+    '''
+    os.system('setfont ' + name)
+
+
+def main():
+    '''The main function of our program'''
+
+    # set the console just how we want it
+    reset_console()
+    set_cursor(OFF)
+    set_font('Lat15-Terminus24x12')
+
+    # display something on the screen of the device
+    print('Hello World!')
+
+    # print something to the output panel in VS Code
+    debug_print('Hello VS Code!')
+
+    # set the motor variables
+    mb = ev3.LargeMotor('outB')
+    mc = ev3.LargeMotor('outC')
+    normalSpeed = 30
+
+
+    # set the ultrasonic sensor variable
+    us3 = ev3.UltrasonicSensor('in3')
+
+    # fetch the distance
+    distance = us3.value()
+
+    # constants
+    kp = 1
+    ki = 0
+    kd = 0
+
+    offset = 500
+    # target power level --> be applied when error=0
+    tp = 0
+    integral = 0
+    lastError = 0
+    derivative = 0
+
+    while (1==1):
+
+        print('Distance =',distance)
+
+        distance = us3.value()
+        debug_print("Distance:" + str(distance))
+        error = distance - offset
+        integral = integral + error
+        derivative = error - lastError
+
+        # turn
+        turn = kp*error
+        Correction = kp*error + ki*integral + kd*derivative
+        Correction = Correction/10
+
+        # the power level of 2 motors
+        debug_print(Correction)
+
+        if distance>50:
+            debug_print('Moving forward...')
+        else:
+            debug_print('Moving Backward...')
+            
+        left = tp + Correction
+        right = tp + Correction
+        mb.run_direct(duty_cycle_sp=right)
+        mc.run_direct(duty_cycle_sp=left)
+      
+
+        time.sleep(1)
+        mb.run_direct(duty_cycle_sp=0)
+        mc.run_direct(duty_cycle_sp=0)
+        lastError = error 
+
+
+
+
+
+    # fetch the distance
+    distance = us3.value()
+    # print the distance to the output panel in VS Code
+    debug_print('Distance =',distance)
+    
+    # announce program end
+    ev3.Sound.speak('Test program ending').wait()
+
+if __name__ == '__main__':
+    main()
